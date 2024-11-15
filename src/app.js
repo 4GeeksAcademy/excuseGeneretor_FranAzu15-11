e2f9794dfa3ebbc2e5d6b4167c890af18bd5b2bb
FranAzuara <franazuara@gmail.com>
2024-11-15 20:07:48 +0000

    generador de excusas hecho

--- src/app.js
+++ src/app.js
@@ -7,5 +7,32 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["The dog", "My grandma", "The mailman", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "my phone", "the car"];
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  function elementoAleatorio(list) {
+    let elemento = "";
+    elemento = list[Math.floor(Math.random() * list.length)];
+    return elemento;
+  }
+  elementoAleatorio(who);
+
+  let texto = document.getElementById("excuse");
+  console.log(texto);
+  texto.innerHTML =
+    elementoAleatorio(who) +
+    " " +
+    elementoAleatorio(action) +
+    " " +
+    elementoAleatorio(what) +
+    " " +
+    elementoAleatorio(when);
 };
